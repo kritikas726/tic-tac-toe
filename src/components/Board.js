8649f10114bd58954fa3bfd928b6b55b657f5740
kritikas726 <kritikas726@gmail.com>
2021-08-14 22:57:10 +0530

    highlight winning sqares and add start new game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Square from './Square';
 
-const Board = ({ board, handleSquareClick }) => {
+const Board = ({ board, handleSquareClick, winnerSquares }) => {
   const renderSquare = position => {
+    const isWinningSquare = winnerSquares.includes(position);
     return (
       <Square
         value={board[position]}
         onClick={() => handleSquareClick(position)}
+        isWinningSquare={isWinningSquare}
       />
     );
   };
